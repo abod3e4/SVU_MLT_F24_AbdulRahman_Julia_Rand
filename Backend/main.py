7e3248cd3dfe15e174c2c971af013c1c1f98cf32
AbdulRahman AlArmashi <abdulrahman.a@merkler00t.com>
2025-06-15 21:42:33 +0300

    Adding the first part of backend functionality

--- Backend/main.py
+++ Backend/main.py
@@ -13,8 +13,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
- )
-
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
